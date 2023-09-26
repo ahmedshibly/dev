5d244ba815327eafa9a1f868ca3fdda1942d2269
Nayem Shibly <nayem.ahmed@wundermanthompson.com>
2023-09-26 17:59:13 +0600

    🚧 WIP : init the module and spinner

---
+++ dev.psm1
@@ -0,0 +1,23 @@
+﻿. $PSScriptRoot\spinner.ps1
+
+function dev {    
+    # param (
+    #     [string]$action,
+    #     [string]$s
+        
+    # )
+
+    # switch ($action) {
+    #     start { 
+    #         Write-Output "Starting: $s"
+    #     }
+    #     Default {}
+    # }
+
+    Write-Host "Calling spinner"
+    start-spinner { Start-Sleep 5 }
+    
+   
+}
+
+Export-ModuleMember -Function dev
---
+++ spinner.ps1
@@ -0,0 +1,36 @@
+﻿function start-spinner($scriptBlock) {
+    
+    $cursorTop = [Console]::CursorTop
+    
+    try {
+        [Console]::CursorVisible = $false
+        
+        $counter = 0
+        # $frames = '|', '/', '-', '\' 
+        # $frames = '┌', '┐', '┘', '└' 
+         $frames = '⠋', '⠙', '⠚', '⠓' 
+        # $frames = '°', '°°', '°°°', '°' 
+        
+        $jobName = Start-Job -ScriptBlock $scriptBlock
+    
+        while($jobName.JobStateInfo.State -eq "Running") {
+            $frame = $frames[$counter % $frames.Length]
+            
+            Write-Host "$frame" -NoNewLine
+            [Console]::SetCursorPosition(0, $cursorTop)
+            
+            $counter += 1
+            Start-Sleep -Milliseconds 125
+        }
+        
+        # Only needed if you use a multiline frames
+        Write-Host ($frames[0] -replace '[^\s+]', ' ')
+    }
+    finally {
+        [Console]::SetCursorPosition(0, $cursorTop)
+        [Console]::CursorVisible = $true
+    }
+}
+
+# Example:
+# start-spinner { Start-Sleep 5 }
